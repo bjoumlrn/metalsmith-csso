9b2e3b102dae9d662e2d531e8485c7b456efce5c
Bjoumlrn <bjoumlrn@github>
2018-04-30 20:05:12 +0200

    Long overdue updates

--- test/index.js
+++ test/index.js
@@ -1,29 +1,68 @@
-var assertDir = require( 'assert-dir-equal' )
-var csso = require( '../' )
-var Metalsmith = require( 'metalsmith' )
+/* eslint-env mocha */
+/* eslint-disable no-sync */
 
-describe( 'metalsmith-csso', function() {
+const tap = require( 'tape' );
+const fs = require( 'fs' );
+const path = require( 'path' );
+const os = require( 'os' );
+const csso = require( '../' );
+const Metalsmith = require( 'metalsmith' );
 
-    it( 'should minimize redundant structure', function( done ) {
-        Metalsmith( 'test/fixtures/basic' )
-            .use( csso() )
-            .build( function( err ) {
-                if ( err ) return done( err )
-                assertDir( 'test/fixtures/basic/expected', 'test/fixtures/basic/build' )
-                return done( null )
-            } );
-    } );
+function createTestDirectory( template ) {
+	const dir = fs.mkdtempSync( path.join(
+		os.tmpdir(), 'metalsmith-csso-' )
+	);
+	fs.mkdirSync( path.join( dir, 'src' ) );
+	fs.copyFileSync(
+		path.join( template, 'src', 'style.css' ),
+		path.join( dir, 'src', 'style.css' )
+	);
+	return dir;
+}
 
-    it( 'should accept the option to disable structure minimization', function( done ) {
-        Metalsmith( 'test/fixtures/structured' )
-            .use( csso( {
-                minimizeStructure: false
-            } ) )
-            .build( function( err ) {
-                if ( err ) return done( err )
-                assertDir( 'test/fixtures/structured/expected', 'test/fixtures/structured/build' )
-                return done( null )
-            } );
-    } );
+tap.test( 'should minimize redundant structure', ( t ) => {
+	const dir = createTestDirectory( 'test/fixtures/basic' );
+	Metalsmith( dir )
+		.use( csso() )
+		.build( ( err ) => {
+			t.equal( err, null, 'no error occured' );
+			t.equal(
+				fs.readFileSync( path.join( dir, 'build', 'style.css' ), 'utf8' ),
+				fs.readFileSync( 'test/fixtures/basic/expected/style.css', 'utf8' ),
+				'output matches expected output'
+			);
+		} );
+	t.end();
+} );
 
+tap.test( 'should accept the option to disable structure minimization', ( t ) => {
+	const dir = createTestDirectory( 'test/fixtures/structured' );
+	Metalsmith( dir )
+		.use( csso( { 'restructure': false } ) )
+		.build( ( err ) => {
+			t.equal( err, null, 'no error occured' );
+			t.equal(
+				fs.readFileSync( path.join( dir, 'build', 'style.css' ), 'utf8' ),
+				fs.readFileSync( 'test/fixtures/structured/expected/style.css', 'utf8' ),
+				'output matches expected output'
+			);
+		} );
+	t.end();
+} );
+
+tap.test( 'should generate source maps when asked to', ( t ) => {
+	const dir = createTestDirectory( 'test/fixtures/sourcemap' );
+	Metalsmith( dir )
+		.use( csso( { 'sourceMap': true } ) )
+		.build( ( err ) => {
+			t.equal( err, null, 'no error occured' );
+			t.equal(
+				fs.readFileSync( path.join( dir, 'build', 'style.css' ), 'utf8' ),
+				fs.readFileSync( 'test/fixtures/sourcemap/expected/style.css', 'utf8' ),
+				'output matches expected output'
+			);
+			const source_map_content = fs.readFileSync( path.join( dir, 'build', 'style.css.map' ), 'utf8' );
+			t.equal( /sourcesContent/.test( source_map_content ), true, 'looks like a source map' );
+		} );
+	t.end();
 } );
